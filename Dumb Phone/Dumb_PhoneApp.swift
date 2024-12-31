e4dfa363e5f7eaafb29ba7f4e83b370ff0fe7d3a
fortytwode <shamanth@rocketshiphq.com>
2024-12-31 14:11:12 +0100

    Initial Commit

---
+++ Dumb Phone/Dumb_PhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dumb_PhoneApp.swift
+//  Dumb Phone
+//
+//  Created by Shamanth Rao on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Dumb_PhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
